8730f31dc77806a0f6ea26b57b0086ea913bab53
ghostupload <78751284+ghostupload@users.noreply.github.com>
2023-04-01 00:36:04 +0800

    feat: 增加错误管理

--- Daily_Picture/pick_a_pic.py
+++ Daily_Picture/pick_a_pic.py
@@ -2,49 +2,65 @@ import os, shutil, datetime, fileinput
 
 def nicepath(dir):
     "①文件夹存在②有jpg③有list④list不为空，都满足则返回1"
-    if os.path.exists(dir) & os.path.exists(dir + '/list.txt'):
-        imgFile = os.listdir(dir)
-        for i in range(len(imgFile)):
-            imgFile[i] = os.path.splitext(imgFile[i])[1]
-        if '.jpg' in imgFile:
-            size = os.path.getsize(dir + '/list.txt')
-            if size != 0:
-               return 1
+    # 检查文件夹是否存在
+    if not os.path.exists(dir):
+        with open('error.log', 'a', encoding='utf-8') as f:
+            f.write(f'Error: folder "{dir}" does not exist.\n')
+        return 0
+    # 检查是否有 jpg 文件
+    imgFiles = [f for f in os.listdir(dir) if f.endswith('.jpg')]
+    if not imgFiles:
+        with open('error.log', 'a', encoding='utf-8') as f:
+            f.write(f'Error: no jpg files found in folder "{dir}".\n')
+        return 0
+    # 检查是否有 list.txt 文件且不为空
+    listFile = os.path.join(dir, 'list.txt')
+    if not os.path.exists(listFile):
+        with open('error.log', 'a', encoding='utf-8') as f:
+            f.write(f'Error: list.txt not found in folder "{dir}".\n')
+        return 0
+    elif os.path.getsize(listFile) == 0:
+        with open('error.log', 'a', encoding='utf-8') as f:
+            f.write(f'Error: list.txt in folder "{dir}" is empty.\n')
+        return 0
+    # 满足所有条件，返回 1
+    return 1
 
 workPath = os.path.abspath(os.path.dirname(__file__))
 os.chdir(workPath)
 
 # Step 1: 确定原图路径：
 
+SPECIAL_DIR = '/special/'
+SCREENSHOTS_DIR = '/screenshots/'
+#这里可以修改special文件夹和默认的普通截图文件夹
+
 if nicepath('special')==1:    # 优先选special
-    imgDir = '/special/'
+    imgDir = SPECIAL_DIR
 else:   # 其次选对应的纪念日（文件夹名格式为MMDD）
     t = datetime.datetime.now()
-    dateDir = t.__format__('%m%d')
+    dateDir = t.strftime('%m%d')
     if nicepath(dateDir)==1:
         imgDir = '/' + dateDir + '/'
     else:
-        imgDir = '/screenshots/'    # 不符合特殊条件就选默认文件夹
+        imgDir = SCREENSHOTS_DIR    # 不符合特殊条件就选默认文件夹
 
 imgPath = workPath + imgDir
 os.chdir(imgPath)   # 切换工作文件夹到原图路径
 
 # Step 2: 移动图片
 
-# 读取list第一行然后删除
-fileName = 'fileName'
-while os.path.exists(fileName) is False: # 如果第一行文件不存在，则删除后再读下一行
-   with open('list.txt', 'r', encoding='utf-8') as f:
-      fileName = f.readline().rstrip()
-   for line in fileinput.input('list.txt', inplace=1):
-      if not fileinput.isfirstline():
-         print(line.replace('\n',''))
-# 移动相应图片并记录
-logContents = imgDir[1:] + fileName + "\t"  + str(datetime.datetime.now())+ "\n"
-shutil.move(imgPath + fileName, workPath + '/img.jpg')
-with open(workPath + '/imgpicked.log', 'a+', encoding='utf-8') as log:
+myList = os.path.join(imgPath, 'list.txt')
+fileName = next(open(myList)).strip()
+while not os.path.exists(fileName):
+    os.system(f'sed -i "1d" {myList}')
+    fileName = next(open(myList)).strip()  # 得到fileName字符串
+
+# 移动jpg文件，记录原图文件夹、文件名和时间
+logContents = f"{imgDir[1:]}{fileName}\t{str(datetime.datetime.now())[:-4]}\n"
+shutil.move(os.path.join(imgPath, fileName), os.path.join(workPath, 'img.jpg'))
+with open(os.path.join(workPath, 'imgpicked.log'), 'a', encoding='utf-8') as log:
     log.write(logContents)
 
-# Step 3: 输出图片描述文alt
-imgAlt = fileName[:-4] # 即文件名去掉最后的'.jpg'
-print(imgAlt)
+# Step 3: 输出图片文件名（去掉.jpg后缀和多余换行符）
+print(fileName[:-4], end='') 
